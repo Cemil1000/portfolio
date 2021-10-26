46ae20710b902d55823cd2f5d2c395714c5761df
cemil <cemilyilmaz.be@gmail.com>
2021-10-26 20:59:41 +0200

    commit

---
+++ components/Landing/Array_landing.js
@@ -0,0 +1,21 @@
+import { FaEnvelope, FaGithub, FaPhoneAlt } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
+
+export const logoSocial = [
+    {
+        href: 'tel:0483701853',
+        typeNameIcon: <FaPhoneAlt className='iconReact' size='2rem' />,
+    },
+    {
+        href: 'mailto:cemilyilmaz.be@gmail.com',
+        typeNameIcon: <FaEnvelope className='iconReact' size='2rem' />,
+    },
+    {
+        href: 'https://www.linkedin.com/in/cemil-yilmaz-b72230184/',
+        typeNameIcon: <AiFillLinkedin className='iconReact' size='2rem' />,
+    },
+    {
+        href: 'https://github.com/Cemil1000',
+        typeNameIcon: <FaGithub className='iconReact' size='2rem' />,
+    },
+]
